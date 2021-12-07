8a729a98d741db964499a917e9ab2834886bfb84
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-12-07 17:15:42 +0300

    Add files via upload

---
+++ Zadanie delegati/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zadanie_delegati
+{
+    class Program
+    {
+        delegate double MyDelegate(double R);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус, затем нажмите Enter");
+            double r = Convert.ToDouble(Console.ReadLine());
+            MyDelegate myDelegate = new MyDelegate(DlinaOkrugnosti);
+            myDelegate += PloshadKruga;
+            myDelegate += ObemShara;
+            myDelegate?.Invoke(r);
+            Console.ReadKey();
+
+
+           static double DlinaOkrugnosti(double R)
+            {
+                double d = 2 * Math.PI * R;
+                Console.WriteLine($"Длина окружности равна {d}");
+                return d;
+
+            }
+            static double PloshadKruga(double R)
+            {
+                double s = Math.PI * Math.Pow(R,2);
+                Console.WriteLine($"Площадь круга равна {s}");
+                return s;
+            }
+            static double ObemShara(double R)
+            {
+                double v = 4/3 * Math.PI * Math.Pow(R, 3);
+                Console.WriteLine($"Объем шара равен {v}");
+                return v;
+            }
+
+
+        }
+    }
+}
